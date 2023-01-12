85bd1736141355adc9d1e6170d2ae1b61faac0aa
agrandesr <agrandesr@gmail.com>
2023-01-12 15:54:23 +0100

    Entities created

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 
-First step, we make the command, following the symfony documentation.
+
+## Command creation
+We make the command, following the symfony documentation.
 ``` bash
 php bin/console make:command
-````
+```
+
+## Creation of the table
+We make the entities, following the symfony documentation.
+``` bash
+php bin/console make:entity
+```
---
+++ src/Entity/Characters.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CharactersRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CharactersRepository::class)]
+class Characters
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $mass = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $height = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $gender = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $picture = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMass(): ?int
+    {
+        return $this->mass;
+    }
+
+    public function setMass(int $mass): self
+    {
+        $this->mass = $mass;
+
+        return $this;
+    }
+
+    public function getHeight(): ?int
+    {
+        return $this->height;
+    }
+
+    public function setHeight(int $height): self
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+}
